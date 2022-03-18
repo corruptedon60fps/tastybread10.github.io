1c8b154eab393133e6ba912c2d9ecbca41b212c2
TastyBread10 <72051172+TastyBread10@users.noreply.github.com>
2022-03-18 16:54:58 -0400

    Final Revamp of the TastyTeam Website...

---
+++ index.js
@@ -0,0 +1,10 @@
+function myFunction() {
+    var element = document.getElementById("mars-instagram");
+    element.classList.add("fa-beat");
+  }
+
+  function alsoMyFunction() {
+    var element = document.getElementById("mars-instagram");
+    element.classList.remove("fa-beat");
+  }
+  
